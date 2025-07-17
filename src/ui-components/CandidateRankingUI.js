a8e97c91e34826dbad5f2b6d4648244be383ccf6
David Streuli <uniqued4ve@gmail.com>
2025-07-17 14:50:40 +0200

    console.log CandidateRankingUI.js

--- src/ui-components/CandidateRankingUI.js
+++ src/ui-components/CandidateRankingUI.js
@@ -19,6 +19,9 @@ export class ActivityDisplay {
     static addFirstChoiceListener(container) {
         container.querySelector('#show-first-choice')?.addEventListener('click', () => {
             const first = this.candidatesData[0];
+            console.log('\n========= this.candidatesData[0] ========= \n\n', result);
+            console.log('\n### </==========================================>');
+            console.log(`${JSON.stringify(result, null, 2)}`);
             if (!first) return;
             
             let display = container.querySelector('.first-choice-display');
